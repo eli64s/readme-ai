da53840e9a7c0bf7d3c09c4618eafd216dcc549d
Eli <43382407+eli64s@users.noreply.github.com>
2023-03-29 14:20:48 -0500

    Enhance setup.sh script for users.

--- setup/setup.sh
+++ setup/setup.sh
@@ -16,14 +16,18 @@ fi
 
 # Check for Python installation and required version (3.7+)
 echo "Checking for Python 3.7 or higher..."
-python_version=$(python3 -c 'import sys; print(sys.version_info[:])' 2>/dev/null)
+python_version=$(python3 -c 'import sys; print("{}.{}".format(sys.version_info[0], sys.version_info[1]))' 2>/dev/null)
 
 if [ $? -ne 0 ]; then
     echo "Python 3 is not installed. Please install Python 3.7 or higher and rerun the script."
     exit 1
 fi
 
-if [ ${python_version:1:1} -lt 3 ] || ([ ${python_version:1:1} -eq 3 ] && [ ${python_version:4:1} -lt 7 ]); then
+IFS='.' read -ra version_parts <<< "$python_version"
+major=${version_parts[0]}
+minor=${version_parts[1]}
+
+if [ "$major" -lt 3 ] || ([ "$major" -eq 3 ] && [ "$minor" -lt 7 ]); then
     echo "Please upgrade to Python 3.7 or higher and rerun the script."
     exit 1
 fi
