b9e3affd6b48fdb04f90a0c16d8f59cdc736ff96
Eli <43382407+eli64s@users.noreply.github.com>
2023-03-29 14:16:56 -0500

    Improved setup.sh script for users.

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,35 +1,55 @@
 #!/bin/bash
 
-# Check if Conda is already installed
-if command -v conda >/dev/null 2>&1; then
-    echo "Conda is already installed."
-else
-    # Download and install Conda
-    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        # Linux
-        wget -qO- https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh | bash
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
-        # Mac OSX
-        curl -o miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
-        bash miniconda.sh -b -p $HOME/miniconda
-    else
-        echo "Unsupported operating system."
-        exit 1
-    fi
+# Clone the repository
+#echo "Cloning the README-AI repository..."
+#git clone https://github.com/eli64s/README-AI.git
+#cd README-AI
+
+# Welcome message
+echo "Welcome to the README-AI environment setup script!"
+
+# Check if conda is installed
+if ! command -v conda &> /dev/null; then
+    echo "Conda is not installed. Please install Anaconda or Miniconda and rerun the script."
+    exit 1
 fi
 
-# Update Conda
-conda update -y conda
+# Check for Python installation and required version (3.7+)
+echo "Checking for Python 3.7 or higher..."
+python_version=$(python3 -c 'import sys; print(sys.version_info[:])' 2>/dev/null)
 
-# Create conda environment
-conda create -n readmeai python=3.9 -y
+if [ $? -ne 0 ]; then
+    echo "Python 3 is not installed. Please install Python 3.7 or higher and rerun the script."
+    exit 1
+fi
 
-# Activate conda environment
+if [ ${python_version:1:1} -lt 3 ] || ([ ${python_version:1:1} -eq 3 ] && [ ${python_version:4:1} -lt 7 ]); then
+    echo "Please upgrade to Python 3.7 or higher and rerun the script."
+    exit 1
+fi
+
+echo "Python version is compatible."
+
+# Create a new conda environment named 'readmeai'
+echo "Creating a new conda environment named 'readmeai' with Python 3.8..."
+conda create -n readmeai python=3.8 -y
+
+# Activate the conda environment
+echo "Activating the 'readmeai' environment..."
 eval "$(conda shell.bash hook)"
 conda activate readmeai
 
-# Install project dependencies
-pip install -e .
+# Install the required packages
+echo "Installing required packages from 'requirements.txt'..."
+pip install -r requirements.txt
+
+echo "Packages installed successfully."
 
 # Download spacy language model
+echo "Downloading the spaCy language model 'en_core_web_sm'..."
 python -m spacy download en_core_web_sm
+
+# Deactivate the conda environment when setup is complete
+conda deactivate
+
+echo "Setup complete. Use 'conda activate readmeai' to activate the environment."
