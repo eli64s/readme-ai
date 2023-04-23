27967415fd7919d2f148201a68dcd47a5860bce1
Eli <43382407+eli64s@users.noreply.github.com>
2023-04-23 07:53:46 -0500

    Enhanced error handling in setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# Clone the repository
-#echo "Cloning the README-AI repository..."
-#git clone https://github.com/eli64s/README-AI.git
-#cd README-AI
-
 # Welcome message
 echo "Welcome to the README-AI environment setup script!"
+
+# Check if git is installed
+if ! command -v git &> /dev/null; then
+    echo "Git is not installed. Please install Git and rerun the script."
+    exit 1
+fi
+
+# Clone the repository
+echo "Cloning the README-AI repository..."
+git clone https://github.com/eli64s/README-AI.git
+cd README-AI
 
 # Check if conda is installed
 if ! command -v conda &> /dev/null; then
@@ -36,7 +42,10 @@ echo "Python version is compatible."
 
 # Create a new conda environment named 'readmeai'
 echo "Creating a new conda environment named 'readmeai' with Python 3.8..."
-conda env create -f setup/environment.yaml
+conda env create -f setup/environment.yaml || {
+    echo "Error creating the 'readmeai' environment. Aborting."
+    exit 1
+}
 
 # Activate the conda environment
 echo "Activating the 'readmeai' environment..."
@@ -49,10 +58,19 @@ export PATH="$(conda info --base)/envs/readmeai/bin:$PATH"
 
 # Install the required packages using pip
 echo "Installing required packages from 'requirements.txt'..."
-pip install -r requirements.txt
+pip install -r requirements.txt || {
+    echo "Error installing packages from 'requirements.txt'. Aborting."
+    conda deactivate
+    exit 1
+}
 
 # Download the spaCy model
-python -m spacy download en_core_web_sm
+echo "Downloading the spaCy model..."
+python -m spacy download en_core_web_sm || {
+    echo "Error downloading the spaCy model. Aborting."
+    conda deactivate
+    exit 1
+}
 
 # Deactivate the conda environment
 conda deactivate
