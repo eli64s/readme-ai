70debab3b2892b8b0f6dd034de667a45676ba52c
Eli <43382407+eli64s@users.noreply.github.com>
2023-05-27 00:40:48 -0500

    Increase unit test coverage ~74% total.

--- tests/test_preprocess.py
+++ tests/test_preprocess.py
@@ -1,9 +1,12 @@
 """Unit tests for preprocess.py."""
 
-import os
-import shutil
 import sys
+
+sys.path.append("src")
+
 from pathlib import Path
+from unittest.mock import patch
+from urllib.parse import urlparse
 
 import git
 import pytest
@@ -15,139 +18,159 @@ from src.preprocess import (
     _get_file_parsers,
     _get_remote_repository,
     _map_extensions_to_languages,
-    extract_dependencies,
+    additional_dependencies,
     get_repository,
     get_repository_name,
 )
 
-sys.path.append("src")
+
+@pytest.fixture
+def setup_files(tmpdir_factory):
+    fn = tmpdir_factory.mktemp("data").join("hello.txt")
+    with open(fn, "w") as f:
+        f.write("content")
+    return str(fn)
 
 
-# Define test constants
-TEST_DIR = os.path.dirname(os.path.abspath(__file__))
-TEMP_DIR = os.path.join(TEST_DIR, "temp_dir")
-TEST_REPO = "https://github.com/example/test_repo.git"
-TEST_FILE_EXT = [".py", ".txt"]
-TEST_FILE_NAMES = ["requirements.txt", "Pipfile"]
+def test_clone_repository(tmp_path):
+    repo_url = "https://github.com/user/repo.git"
+    hosts = [urlparse(repo_url).hostname]
+    with patch.object(git.Repo, "clone_from") as mocked_clone_from:
+        _clone_or_copy_repository(hosts, repo_url, tmp_path)
+    mocked_clone_from.assert_called_once_with(repo_url, tmp_path)
 
 
-# Fixture to create a temporary directory
-@pytest.fixture(scope="module")
-def temp_directory():
-    os.makedirs(TEMP_DIR)
-    yield
-    shutil.rmtree(TEMP_DIR)
+def test_copy_repository(tmp_path):
+    source_dir = tmp_path / "source_dir"
+    source_dir.mkdir()
+    (source_dir / "file.txt").touch()
+    dest_dir = tmp_path / "dest_dir"
+
+    hosts = ["github.com"]
+    _clone_or_copy_repository(hosts, str(source_dir), str(dest_dir))
+
+    assert (dest_dir / "file.txt").exists()
 
 
-# Tests for helper functions
+def test_copy_repository_dest_dir_exists(tmp_path):
+    source_dir = tmp_path / "source_dir"
+    source_dir.mkdir()
+    (source_dir / "file.txt").touch()
+    dest_dir = tmp_path / "dest_dir"
+    dest_dir.mkdir()
+    (dest_dir / "old_file.txt").touch()
+
+    hosts = ["github.com"]
+    _clone_or_copy_repository(hosts, str(source_dir), str(dest_dir))
+
+    assert not (dest_dir / "old_file.txt").exists()
+    assert (dest_dir / "file.txt").exists()
 
 
-def test_clone_or_copy_repository_remote(config, temp_directory):
-    # Test cloning a remote repository
-    _clone_or_copy_repository(config["git"]["hosts"], TEST_REPO, TEMP_DIR)
-    assert os.path.exists(os.path.join(TEMP_DIR, ".git"))
+def test_invalid_source(tmp_path):
+    invalid_path = "/invalid/path"
+    hosts = ["github.com"]
+    with pytest.raises(
+        ValueError, match="Repository path or URL is not valid."
+    ):
+        _clone_or_copy_repository(hosts, invalid_path, tmp_path)
 
 
-def test_clone_or_copy_repository_local(config, temp_directory):
-    # Test copying a local directory
-    local_dir = os.path.join(TEST_DIR, "test_local_dir")
-    os.makedirs(local_dir)
-    with open(os.path.join(local_dir, "test_file.txt"), "w") as f:
-        f.write("Test file")
-    _clone_or_copy_repository(config["git"]["hosts"], local_dir, TEMP_DIR)
-    assert os.path.exists(os.path.join(TEMP_DIR, "test_file.txt"))
+def test_extract_repository_contents(tmp_path):
+    # Arrange
+    (tmp_path / "test.txt").write_text("hello world!")
+    (tmp_path / "test2.txt").write_text("hello again!")
+    (tmp_path / ".git").mkdir()
+    (tmp_path / ".git" / "config").write_text(
+        "[core]\n    repositoryformatversion = 0"
+    )
+    contents = _extract_repository_contents(str(tmp_path))
+    assert contents == {
+        Path("test.txt"): "hello world!",
+        Path("test2.txt"): "hello again!",
+    }
 
 
-def test_get_remote_repository(temp_directory, monkeypatch):
-    # Test getting codebase from a remote repository
-    def mock_clone_from(url, temp_dir, depth=1):
-        os.makedirs(os.path.join(temp_dir, "subdir"))
-        with open(os.path.join(temp_dir, "subdir", "file.txt"), "w") as f:
-            f.write("Test file")
-
-    monkeypatch.setattr(git.Repo, "clone_from", mock_clone_from)
-
-    codebase = _get_remote_repository(TEST_REPO)
-    assert "subdir/file.txt" in str(codebase)
-    assert codebase["subdir/file.txt"] == "Test file"
+def test_extract_repository_contents_with_non_utf8(tmp_path):
+    (tmp_path / "test.txt").write_bytes(b"\x80abc")  # non-UTF8 content
+    contents = _extract_repository_contents(str(tmp_path))
+    assert contents == {
+        Path(
+            "test.txt"
+        ): "Could not decode content: non-text or non-UTF-8 file."
+    }
 
 
-def test_get_repository_local(temp_directory):
-    # Test getting codebase from a local directory
-    local_dir = os.path.join(TEST_DIR, "test_local_dir")
-    os.makedirs(local_dir)
-    with open(os.path.join(local_dir, "test_file.txt"), "w") as f:
-        f.write("Test file")
-
-    codebase = get_repository(local_dir)
-    assert "test_file.txt" in codebase
-    assert codebase["test_file.txt"] == "Test file"
+def test_extract_repository_contents_empty_directory(tmp_path):
+    contents = _extract_repository_contents(str(tmp_path))
+    assert contents == {}
 
 
-def test_extract_repository_contents(temp_directory):
-    # Test getting file contents from a directory
-    os.makedirs(os.path.join(TEMP_DIR, "subdir"))
-    with open(os.path.join(TEMP_DIR, "subdir", "file.txt"), "w") as f:
-        f.write("Test file")
-
-    contents = _extract_repository_contents(TEMP_DIR)
-    assert Path("subdir/file.txt") in contents
-    assert contents["subdir/file.txt"] == "Test file"
+def test_get_file_extensions():
+    files = ["file1.txt", "file2.py", "file3.py", "file4.java"]
+    expected_result = ["txt", "py", "java"]
+    assert sorted(_get_file_extensions(files)) == sorted(expected_result)
 
 
-def test_extract_programming_language():
-    # Test getting file extensions
-    all_files = ["file1.py", "file2.txt", "file3.js"]
-    file_ext = {".py": ".py", ".js": ".js"}
-
-    file_extensions = _get_file_extensions(all_files, file_ext)
-    assert file_extensions[0] in ["py", "js", "txt"]
-    assert file_extensions[1] in ["py", "js", "txt"]
-    assert file_extensions[2] in ["py", "js", "txt"]
+def test_map_extensions_to_languages():
+    ext_list = ["py", "js", "java", "txt"]
+    languages = {
+        "py": "Python",
+        "js": "JavaScript",
+        "java": "Java",
+    }
+    expected_result = [
+        "py",
+        "js",
+        "java",
+        "txt",
+        "Python",
+        "JavaScript",
+        "Java",
+    ]
+    assert sorted(_map_extensions_to_languages(ext_list, languages)) == sorted(
+        expected_result
+    )
 
 
 def test_get_file_parsers():
-    # Test getting file parsers
-    file_parsers = _get_file_parsers()
-    assert callable(file_parsers["requirements.txt"])
-    assert callable(file_parsers["Pipfile"])
+    parsers = _get_file_parsers()
+    assert isinstance(parsers, dict)
+    assert "build.gradle" in parsers
 
 
-# Tests for main functions
+def test_get_remote_repository(mocker):
+    mocker.patch("git.Repo.clone_from")
+    mocker.patch("tempfile.TemporaryDirectory")
+    mocker.patch("src.preprocess._extract_repository_contents")
+
+    assert _get_remote_repository("https://test-repo-url.com") is not None
 
 
-def test_get_repository_invalid_repo(temp_directory):
-    # Test getting codebase from an invalid repository
-    codebase = get_repository("invalid_repo")
-    assert codebase == {}
+def test_get_repository(mocker):
+    mocker.patch("src.preprocess.utils.valid_url", return_value=True)
+    mocker.patch("src.preprocess._get_remote_repository")
+    assert get_repository("https://test-repo-url.com") is not None
+
+    mocker.patch("src.preprocess.utils.valid_url", return_value=False)
+    mocker.patch("src.preprocess._extract_repository_contents")
+    assert get_repository("/local/path/to/repo") is not None
 
 
-def test_extract_dependencies(config, temp_directory, monkeypatch):
-    # Test getting project dependencies
-    def mock_clone_or_copy_repository(repo, temp_dir):
-        # Create dummy dependency files
-        for file_name in TEST_FILE_NAMES:
-            with open(os.path.join(temp_dir, file_name), "w") as f:
-                f.write("Test dependency")
-
-    monkeypatch.setattr(
-        "preprocess._clone_or_copy_repository", mock_clone_or_copy_repository
+def test_get_repository_name():
+    hosts = ["github.com", "gitlab.com"]
+    assert (
+        get_repository_name(hosts, "https://github.com/user/repo.git")
+        == "repo"
     )
-    dependencies = extract_dependencies(
-        TEST_REPO, TEST_FILE_EXT, TEST_FILE_NAMES
-    )
-    assert dependencies == ["Test dependency"] * 2 + TEST_FILE_EXT
+    assert get_repository_name(hosts, "/local/path/to/repo") == "repo"
 
 
-def test_get_repository_name_remote(config):
-    # Test getting repository name from a remote URL
-    repo_name = get_repository_name(config["git"]["hosts"], TEST_REPO)
-    assert repo_name == "test_repo"
-
-
-def test_get_repository_name_local(config):
-    # Test getting repository name from a local path
-    local_path = os.path.join(TEST_DIR, "local_dir")
-    os.makedirs(local_path)
-    repo_name = get_repository_name(config["git"]["hosts"], local_path)
-    assert repo_name == "local_dir"
+def test_additional_dependencies():
+    files = [
+        Path("docker/compose.yml"),
+        Path(".github/workflows/main.yml"),
+        Path("src/app.py"),
+    ]
+    expected_result = ["docker", "github actions"]
+    assert sorted(additional_dependencies(files)) == sorted(expected_result)
