217ba5498175bd0813bec69609add3c6f4db8442
Eli <43382407+eli64s@users.noreply.github.com>
2023-12-03 21:52:34 -0600

    Update test coverage codecov action.

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,15 +3,11 @@
 eval "$(conda shell.bash hook)"
 conda activate readmeai
 
-# Set the directories to include in the coverage report
 source_dir="readmeai"
-
-# Set the directories to exclude from the coverage report
 omit_dir="tests"
-
-# Exclude __init__.py specifically
 omit_file="readmeai/__init__.py,readmeai/*/__init__.py"
 
 # Generate the coverage report and save it to a file
+#pytest --cov=./ --cov-report=xml
 coverage run --source="$source_dir" --omit="$omit_dir" --omit="$omit_file" -m pytest -v
 coverage report --show-missing --fail-under=90
