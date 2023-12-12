110665c0b7a3074e621b77e3ba444649e1671405
Eli <43382407+eli64s@users.noreply.github.com>
2023-12-12 04:57:32 -0600

    Update test and coverage commands.

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,7 +7,6 @@ source_dir="readmeai"
 omit_dir="tests"
 omit_file="readmeai/__init__.py,readmeai/*/__init__.py"
 
-# Generate the coverage report and save it to a file
-#pytest --cov=./ --cov-report=xml
+# pytest --cov=./ --cov-report=xml --cov-report=term-missing --cov-fail-under=100
 coverage run --source="$source_dir" --omit="$omit_dir" --omit="$omit_file" -m pytest -v
 coverage report --show-missing --fail-under=90
