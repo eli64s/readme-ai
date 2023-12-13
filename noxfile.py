c3820bef871396e88a62d195353ffeb6d22e0f2e
Eli <43382407+eli64s@users.noreply.github.com>
2023-12-12 22:05:34 -0600

    Add noxfile to test cli against multiple py versions.

---
+++ noxfile.py
@@ -0,0 +1,28 @@
+"""Noxfile test automation script."""
+
+import nox
+
+
+def install(session, groups, root=True):
+    """Install the package in the current session."""
+    if root:
+        groups = ["main", *groups]
+
+    session.run_always(
+        "poetry",
+        "install",
+        "--no-root",
+        "--sync",
+        f"--only={','.join(groups)}",
+        external=True,
+    )
+    if root:
+        session.install(".")
+
+
+@nox.session(python=["3.9", "3.10", "3.11", "3.12"])
+def tests(session):
+    """Run the test suite across Python versions"""
+    session.install(".")
+    session.install(".[test]")
+    session.run("pytest", "--cov=./", "--cov-report=term-missing")
