1ffb40d99a5749731678b9ba817cf72af163ea1e
Eli <43382407+eli64s@users.noreply.github.com>
2024-03-10 00:49:13 -0600

    Update test to remove mock generated png image.

--- tests/models/test_dalle.py
+++ tests/models/test_dalle.py
@@ -2,16 +2,28 @@
 Tests for multi-modal models and image generation.
 """
 
+import re
+from pathlib import Path
 from unittest.mock import MagicMock, patch
 
 from readmeai.cli.options import ImageOptions
 from readmeai.models.dalle import DalleHandler
 
 
+def cleanup_mock_png_files(directory="."):
+    """Cleanup mock png files."""
+    pattern = re.compile(
+        r"<MagicMock name='mock\.config\.git\.name' id='\d+'>\.png"
+    )
+    for filename in Path(directory).glob("*.png"):
+        if pattern.match(filename.name):
+            filename.unlink()
+
+
 @patch("readmeai.models.dalle.Client")
 @patch.dict("os.environ", {"OPENAI_API_KEY": "sk-test-key"}, clear=True)
 def test_image_generator(mock_client):
-    """Test image generation."""
+    """Test image generator."""
     # Given
     config = MagicMock()
     image_generator = DalleHandler(config)
@@ -19,10 +31,16 @@ def test_image_generator(mock_client):
     image_generator.client.images.run = MagicMock(
         return_value=MagicMock(data=[MagicMock(url="url")])
     )
+
     # When
     result = image_generator.run()
+
     # Then
     assert result is not None
+    assert isinstance(result, MagicMock)
+
+    # Cleanup
+    cleanup_mock_png_files()
 
 
 @patch("readmeai.models.dalle.get")
@@ -37,17 +55,23 @@ def test_image_download(mock_get):
     mock_response.status_code = 200
     mock_response.content = b"image"
     mock_get.return_value = mock_response
+
     # When
     result = image_generator.download("url")
+
     # Then
     assert isinstance(result, str)
     image_generator._logger.error.assert_not_called()
+
+    # Cleanup
+    cleanup_mock_png_files()
 
 
 @patch("readmeai.models.dalle.get")
 @patch.dict("os.environ", {"OPENAI_API_KEY": "sk-test-key"}, clear=True)
 def test_image_download_failure(mock_get):
     """Test image download failure."""
+
     # Given
     config = MagicMock()
     image_generator = DalleHandler(config)
@@ -71,3 +95,6 @@ def test_image_download_failure(mock_get):
         mock_response.status_code = 200
         result = image_generator.download("url")
         assert result == ImageOptions.BLUE.value
+
+    # Cleanup
+    cleanup_mock_png_files()
